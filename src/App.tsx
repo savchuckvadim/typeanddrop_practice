ef40573242573c62c44442fffc442f624eef21e4
savchuckvadim <savchuckvadim@gmail.com>
2022-06-05 11:05:41 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const order = [3, 1, 2, 4]
+  const initialCardList = order.map((order, index) => (
+    { id: index, order: order, text: `Карточка ${order}` }
+  ))
+
+
+  const [cardList, setCardList] = useState(initialCardList)
+  const dragStartHandler = (e: React.SyntheticEvent<EventTarget>, card: object) => {
+    console.log(card) //что схватили
+  }
+  const dragLeaveHandler = (e: React.SyntheticEvent<EventTarget>) => {
+
+  }
+  const dragEndHandler = (e: React.SyntheticEvent<EventTarget>) => {
+
+  }
+  const dragOverHandler = (e: React.SyntheticEvent<EventTarget>) => {
+    e.preventDefault()
+  }
+  const dropHandler = (e: React.SyntheticEvent<EventTarget>, card: object) => {
+    e.preventDefault()
+    console.log(card) //куда бросили
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        {cardList.map(card =>
+          <div className='card'
+            draggable={true}
+            onDragStart={(e) => { dragStartHandler(e, card) }} //в тот момент когда мы взяли карточку
+            onDragLeave={(e) => { dragLeaveHandler(e) }} //если вышли за пределы другой карточки
+            onDragEnd={(e) => { dragEndHandler(e) }} //если мы отпустили перемещение
+            onDragOver={(e) => { dragOverHandler(e) }} //если мы находимся над каким-то другим объектом
+            onDrop={(e) => { dropHandler(e, card) }} //если мы отпустили карточку и расчитываем, что после этого должно произойти какое-то действие
+          >
+            {card.text}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
